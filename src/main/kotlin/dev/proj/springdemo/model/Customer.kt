10446046c5ea46351f464510a9c0e8ef0368cbcf
AdoniasAlcantara <adoniasasneto@gmail.com>
2020-04-22 13:04:57 -0300

    Added FlyWay for database migration

---
+++ src/main/kotlin/dev/proj/springdemo/model/Customer.kt
@@ -0,0 +1,16 @@
+package dev.proj.springdemo.model
+
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType.IDENTITY
+import javax.persistence.Id
+
+@Entity
+data class Customer(
+        @Id
+        @GeneratedValue(strategy = IDENTITY)
+        val id: Long,
+        val name: String,
+        val email: String,
+        val phone: String
+)
---
+++ src/main/resources/db/migration/V001__create_customer_table.sql
@@ -0,0 +1,11 @@
+create table Customer
+(
+	id bigint auto_increment,
+	name varchar(60) not null,
+	email varchar(255) not null,
+	phone varchar(20) not null,
+	constraint Customer_pk primary key (id)
+);
+
+
+
