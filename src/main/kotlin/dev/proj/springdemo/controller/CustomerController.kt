2f1095deace9e49d329351e975404af536c0bcce
AdoniasAlcantara <adoniasasneto@gmail.com>
2020-04-26 15:00:38 -0300

    Cleanup

--- src/main/kotlin/dev/proj/springdemo/controller/CustomerController.kt
+++ src/main/kotlin/dev/proj/springdemo/controller/CustomerController.kt
@@ -2,9 +2,8 @@ package dev.proj.springdemo.controller
 
 import dev.proj.springdemo.request.CustomerRequest
 import dev.proj.springdemo.service.CustomerService
-import dev.proj.springdemo.util.noContent
 import org.springframework.http.HttpStatus.CREATED
-import org.springframework.http.ResponseEntity
+import org.springframework.http.HttpStatus.NO_CONTENT
 import org.springframework.web.bind.annotation.*
 import javax.validation.Valid
 
@@ -26,8 +25,6 @@ class CustomerController(private val service: CustomerService) {
     fun update(@PathVariable id: Long, @Valid @RequestBody request: CustomerRequest) = service.update(id, request)
 
     @DeleteMapping("/{id}")
-    fun remove(@PathVariable id: Long): ResponseEntity<Nothing>  {
-        service.remove(id)
-        return noContent()
-    }
+    @ResponseStatus(NO_CONTENT)
+    fun remove(@PathVariable id: Long) = service.remove(id)
 }
--- src/main/kotlin/dev/proj/springdemo/util/ResponseUtil.kt
+++
@@ -1,9 +0,0 @@
-package dev.proj.springdemo.util
-
-import org.springframework.http.ResponseEntity
-
-fun <T> ok(body: T) = ResponseEntity.ok(body)
-
-fun <T> notFound() = ResponseEntity.notFound().build<T>()
-
-fun <T> noContent() = ResponseEntity.noContent().build<T>()
